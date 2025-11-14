7cfc2dcf4cbdf5b7eb0175cb63d7f56cc7177807
Shree Kumar <shree181203@gmail.com>
2025-11-14 15:37:01 +0530

    third commit

--- src/test/java/com/movieticket/repository/BookingRepositoryTest.java
+++ src/test/java/com/movieticket/repository/BookingRepositoryTest.java
@@ -4,6 +4,7 @@ import com.movieticket.model.Booking;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 
 import static org.junit.jupiter.api.Assertions.*;
